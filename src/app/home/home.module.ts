ba4df184bea16b39db85ba4567dce338695f9191
unknown <jeantituana@hotmail.com>
2023-12-06 14:43:10 -0500

    storage firebase

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import {FormatFileSizePipe} from './format-file-size.pipe';
 
 
 @NgModule({
@@ -14,6 +15,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage,
+  FormatFileSizePipe
+  ]
 })
 export class HomePageModule {}
